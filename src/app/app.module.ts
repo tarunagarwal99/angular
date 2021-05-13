16f60ec79d1f42929a66a5dfa64f6ab459ca7488
Tarun Prahladka <tarunprahladka965@gmail.com>
2021-05-13 16:12:51 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { AboutComponent } from './MyComponents/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
